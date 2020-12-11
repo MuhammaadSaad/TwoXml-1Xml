934d63435956120fe7b2f671051e212e63b8fa29
muhammad saad siddique <muhammadsaad2387@gmail.com>
2020-12-11 15:56:54 +0500

    Update cnvert.php

--- cnvert.php
+++ cnvert.php
@@ -25,17 +25,17 @@ $dom->xmlVersion = '1.0';
 
 $dom->formatOutput = true;
 
-$xml_file_name = 'productes.xml';
+$xml_file_name = 'products.xml';
 $root = $dom->createElement('items');
-
+$i=0;
 foreach($xml->children() as $item) {
 	$item_node = $dom->createElement('item');
 	//$track = $xml->addChild('item');
 	  $item_node->appendChild($dom->createElement('number', $item->number ));
 	  $nameCZ=strval($item->nameCZ);
-	  $item_node->appendChild($dom->createElement('nameCZ',  utf8_encode($nameCZ) ));
+	  $item_node->appendChild($dom->createElement('nameCZ',  htmlspecialchars($nameCZ) ));
 	    $description=strval($item->description);
-	$item_node->appendChild($dom->createElement('description', $description));
+	$item_node->appendChild($dom->createElement('description', htmlspecialchars($description)));
 	 $manufacturer=strval($item->manufacturer);
 	 $item_node->appendChild(  $dom->createElement('manufacturer',$manufacturer));
 	$item_node->appendChild( $dom->createElement('barcode', strval($item->barcode) ));
@@ -44,11 +44,11 @@ foreach($xml->children() as $item) {
 	  $item_node->appendChild( $dom->createElement('stock', chkStock((int)$item->number)));
 	  #die();
 	  $root->appendChild($item_node);
-
+	  $i++;
 }
 $dom->appendChild($root);
 
 	$dom->save($xml_file_name);
 
-	echo "$xml_file_name has been successfully created";
+	echo "$i items to $xml_file_name has been successfully created";
 ?>
