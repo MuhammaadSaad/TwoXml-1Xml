23e89bb91fde7e534a48eef102578b4f3c828836
muhammad saad siddique <muhammadsaad2387@gmail.com>
2020-12-22 20:03:12 +0500

    Add files via upload

--- cnvert.php
+++ cnvert.php
@@ -1,6 +1,16 @@
 <?php
 
-function chkStock(int $val) {
+$stocks=simplexml_load_file("stock.xml") or die("Error: Cannot create object");
+
+foreach($stocks->children() as $item) {
+		
+		$_stocks["" . $item->sku . ""] = $item->stock;
+
+}
+
+/*
+function chkStock( $val ) { // int $val
+
 	$stocks=simplexml_load_file("stock.xml") or die("Error: Cannot create object");
 	#echo $val;
 	foreach($stocks->children() as $item) {
@@ -11,8 +21,10 @@ function chkStock(int $val) {
 
  		}
 	}
-  return 0;
+
+	return 0;
 }
+*/
 
 $xml=simplexml_load_file("items.xml") or die("Error: Cannot create object");
 
@@ -28,27 +40,34 @@ $dom->formatOutput = true;
 $xml_file_name = 'products.xml';
 $root = $dom->createElement('items');
 $i=0;
+
 foreach($xml->children() as $item) {
+
 	$item_node = $dom->createElement('item');
 	//$track = $xml->addChild('item');
-	  $item_node->appendChild($dom->createElement('number', $item->number ));
-	  $nameCZ=strval($item->nameCZ);
-	  $item_node->appendChild($dom->createElement('nameCZ',  htmlspecialchars($nameCZ) ));
-	    $description=strval($item->description);
+	$item_node->appendChild($dom->createElement('number', $item->number ));
+	$nameCZ=strval($item->nameCZ);
+	$item_node->appendChild($dom->createElement('nameCZ',  htmlspecialchars($nameCZ) ));
+	$description=strval($item->description);
 	$item_node->appendChild($dom->createElement('description', htmlspecialchars($description)));
-	 $manufacturer=strval($item->manufacturer);
-	 $item_node->appendChild(  $dom->createElement('manufacturer',$manufacturer));
+	$manufacturer=strval($item->manufacturer);
+	$item_node->appendChild(  $dom->createElement('manufacturer',$manufacturer));
 	$item_node->appendChild( $dom->createElement('barcode', strval($item->barcode) ));
-	  $item_node->appendChild($dom->createElement('image', strval($item->image)));
-	  #$no=(int)$item->number;
-	  $item_node->appendChild( $dom->createElement('stock', chkStock((int)$item->number)));
-	  #die();
-	  $root->appendChild($item_node);
-	  $i++;
+	$item_node->appendChild($dom->createElement('image', strval($item->image)));
+	#$no=(int)$item->number;
+	$item_node->appendChild( $dom->createElement('stock', $_stocks["" . $item->number . ""] ) );
+	// $item_node->appendChild( $dom->createElement('stock', chkStock($item->number)));
+	// $item_node->appendChild( $dom->createElement('stock', chkStock((int)$item->number)));
+	#die();
+	$root->appendChild($item_node);
+	$i++;
+
 }
+
 $dom->appendChild($root);
 
-	$dom->save($xml_file_name);
+$dom->save($xml_file_name);
 
-	echo "$i items to $xml_file_name has been successfully created";
+echo "$i items to $xml_file_name has been successfully created";
+
 ?>
